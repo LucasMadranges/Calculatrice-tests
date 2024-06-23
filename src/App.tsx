08f514182465308d969150b5c5dfa64dfc817fed
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-06-23 18:29:37 +0200

    feat: calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import Button from "./components/Button.tsx";
+import {useRef} from "react";
 
 function App() {
     const arrNmbr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    const arrCal = ["+", "-", "*", "/"];
+    const arrCal = ["+", "-", "*", "/", "+", "=", "C"];
+
+    const refDisplay = useRef(null);
 
     return (
         <>
             <div className="p-4 border rounded-2xl w-[250px] border-blue-300 m-auto flex flex-col items-center justify-center">
-                <div className="w-full bg-gray-300 mb-4 p-4 rounded-lg">
-                    Text
-                </div>
+                <input id="displayed"
+                       ref={refDisplay}
+                       className="w-full bg-gray-300 mb-4 p-4 rounded-lg h-[56px]"
+                       readOnly={true}>
+                </input>
                 <div className="grid grid-cols-3 mb-4 gap-2">
-                    {arrNmbr.map((number, key) => <Button key={key}>{number}</Button>)}
+                    {arrNmbr.map((number, key) => <Button key={key}
+                                                          display={refDisplay}>{number}</Button>)}
                 </div>
                 <div className="grid grid-cols-3 gap-2">
-                    {arrCal.map((btn, key) => <Button key={key}>{btn}</Button>)}
+                    {arrCal.map((btn, key) => <Button key={key}
+                                                      display={refDisplay}>{btn}</Button>)}
                 </div>
             </div>
         </>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,75 @@
-export default function Button({children}) {
+import {calculation} from "../utils/calculation.ts";
+
+export default function Button({children, display}: { children: any, display: any }) {
     return (
-        <button className="bg-gray-300 p-2 rounded-lg w-10 hover:bg-gray-400 transition">
+        <button className="bg-gray-300 p-2 rounded-lg w-10 hover:bg-gray-400 transition"
+                onClick={(e: any) => {
+                    if (e.target.innerText === "C") {
+                        display.current.value = "";
+                    } else if (e.target.innerText === "=") {
+                        let parts = display.current.value.split(/([-+/*])/);
+                        let val1, val2, operator;
+
+                        parts.map((item: any) => {
+                            if (item === "") {
+                                const index = parts.indexOf("");
+                                if (index > -1) { // only splice array when item is found
+                                    parts.splice(index, 1); // 2nd parameter means remove one item only
+                                }
+                            }
+                        });
+
+                        console.log(parts);
+
+                        if (parts[0] === "-" && parts[3] !== "-") {
+                            val1 = `-${parts[1]}`;
+                            operator = parts[2];
+                            val2 = parts[3];
+                        } else if (parts[0] !== "-" && parts[2] === "-") {
+                            val1 = parts[0];
+                            operator = parts[1];
+                            val2 = `-${parts[3]}`;
+                        } else if (parts[0] === "-" && parts[3] === "-") {
+                            val1 = `-${parts[1]}`;
+                            operator = parts[2];
+                            val2 = `-${parts[4]}`;
+                        } else if (isNaN(parts[0]) && parts[0] !== "-") {
+                            val1 = parts[1];
+                            operator = parts[2];
+                            val2 = parts[3];
+                        } else if (isNaN(parts[2]) && parts[2] !== "-") {
+                            val1 = parts[0];
+                            operator = parts[1];
+                            val2 = parts[3];
+                        } else if ((isNaN(parts[2]) && parts[2] !== "-") && (isNaN(parts[0]) && parts[0])) {
+                            val1 = parts[1];
+                            operator = parts[2];
+                            val2 = parts[4];
+                        } else {
+                            val1 = parts[0];
+                            operator = parts[1];
+                            val2 = parts[2];
+                        }
+
+                        switch (operator) {
+                            case "+":
+                                display.current.value = calculation(Number(val1), Number(val2), "sum");
+                                break;
+                            case "-":
+                                display.current.value = calculation(Number(val1), Number(val2), "sub");
+                                break;
+                            case "*":
+                                display.current.value = calculation(Number(val1), Number(val2), "mul");
+                                break;
+                            case "/":
+                                display.current.value = calculation(Number(val1), Number(val2), "div");
+                                break;
+                        }
+
+                    } else {
+                        display.current.value += children;
+                    }
+                }}>
             {children}
         </button>
     );
